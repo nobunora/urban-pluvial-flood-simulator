4ee0ed702b9e8d9da9fba5d1457e21deb42d0107
nobunora <80997778+nobunora@users.noreply.github.com>
2026-09-20 22:21:28 +0900

    chore: invalidate prepared cache for complete building obstacles

--- floodsim/storage/prepared_grid_cache.py
+++ floodsim/storage/prepared_grid_cache.py
@@ -18,7 +18,7 @@ from floodsim.preprocessing.roof_rainfall import RoofRainAllocation
 from floodsim.storage.run_store import atomic_write_json
 
 PREPARED_GRID_CACHE_SCHEMA = "1"
-PREPARED_GRID_REVISION = "full1m-preprocess-v2-building-supplement"
+PREPARED_GRID_REVISION = "full1m-preprocess-v3-all-building-obstacles"
 
 
 @dataclass(frozen=True)
